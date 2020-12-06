1103f2a0e34068184d1de8767c157df97ca96f1e
Eric van Wijk <eric@van-wijk.com>
2020-12-06 10:50:39 +0100

    Provider config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,8 +18,6 @@ terraform {
 # This provider block uses the following environment variables: 
 # ARM_SUBSCRIPTION_ID, ARM_CLIENT_ID, ARM_CLIENT_SECRET and ARM_TENANT_ID
 provider azurerm {
-    # Pin Terraform version
-    # Pipelines vdc-terraform-apply-ci/cd have a parameter unpinTerraformProviders ('=' -> '~>') to test forward compatibility
     features {
         virtual_machine {
             # Don't do this in production
@@ -30,16 +28,12 @@ provider azurerm {
 
 # Use AKS to prepare Helm provider
 provider helm {
-  # debug                        = true
-
   kubernetes {
     host                       = var.deploy_aks ? module.aks.0.kubernetes_host : ""
     client_certificate         = var.deploy_aks ? base64decode(module.aks.0.kubernetes_client_certificate) : ""
     client_key                 = var.deploy_aks ? base64decode(module.aks.0.kubernetes_client_key) : ""
     cluster_ca_certificate     = var.deploy_aks ? base64decode(module.aks.0.kubernetes_cluster_ca_certificate) : ""
   }
-
-  # override                     = ["spec.template.spec.automountserviceaccounttoken=true"]
 }
 
 # Use AKS to prepare Kubernetes provider
