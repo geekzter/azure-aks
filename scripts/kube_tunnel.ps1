d43f4a6c607a3cb77b5f3f35680c5df17a623f3b
Eric van Wijk <eric@van-wijk.com>
2020-12-20 17:09:29 +0100

    Terraform 0.14 fixes (#5)

--- scripts/kube_tunnel.ps1
+++ scripts/kube_tunnel.ps1
@@ -6,12 +6,12 @@ Set-Environment
 try {
     ChangeTo-TerraformDirectory
 
-    kubectl config use-context $(terraform output aks_name)
+    kubectl config use-context (Get-TerraformOutput aks_name)
 
     # Open SSH tunnel for local portal on 127.0.0.1:8001
     Get-Job | Where-Object {$_.Command.Contains("kubectl proxy")} | Stop-Job
     kubectl proxy &
-    #az aks browse --resource-group $(terraform output resource_group) --name $(terraform output aks_name)
+    #az aks browse --resource-group (Get-TerraformOutput resource_group) --name (Get-TerraformOutput aks_name)
 
     # Wait for agent nodes to have started
     Start-Agents
