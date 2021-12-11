ef856c026d297a052edb199b1f93f8edcc29acef
Eric van Wijk <eric@van-wijk.com>
2021-12-11 10:53:02 +0100

    Internal Load Balancer provisioned by application

--- terraform/modules/aks-network/ingress.tf
+++ terraform/modules/aks-network/ingress.tf
@@ -1,30 +1,30 @@
-# Azure Internal Load Balancer
-resource kubernetes_service internal_load_balancer {
-  metadata {
-    annotations                = {
-      "service.beta.kubernetes.io/azure-load-balancer-internal" = "true"
-    }
-    name                       = "azure-all-front"
-  }
-  spec {
-    selector                   = {
-      app                      = "azure-all-front"
-    }
-    session_affinity           = "ClientIP"
-    port {
-      port                     = 80
-    }
+# Azure Internal Load Balancer provisioned by application (manifest)
+# resource kubernetes_service internal_load_balancer {
+#   metadata {
+#     annotations                = {
+#       "service.beta.kubernetes.io/azure-load-balancer-internal" = "true"
+#     }
+#     name                       = "azure-all-front"
+#   }
+#   spec {
+#     selector                   = {
+#       app                      = "azure-all-front"
+#     }
+#     session_affinity           = "ClientIP"
+#     port {
+#       port                     = 80
+#     }
 
-    type                       = "LoadBalancer"
-  }
+#     type                       = "LoadBalancer"
+#   }
 
-  depends_on                   = [
-    azurerm_firewall_network_rule_collection.iag_net_outbound_rules,
-    azurerm_firewall_application_rule_collection.aks_app_rules,
-    azurerm_private_dns_zone_virtual_network_link.api_server_domain,
-  ]
+#   depends_on                   = [
+#     azurerm_firewall_network_rule_collection.iag_net_outbound_rules,
+#     azurerm_firewall_application_rule_collection.aks_app_rules,
+#     azurerm_private_dns_zone_virtual_network_link.api_server_domain,
+#   ]
 
-  count                        = var.peer_network_id != "" ? 1 : 0
-}
+#   count                        = var.peer_network_id != "" ? 1 : 0
+# }
 
 # Application Ingress controller is created as AKS add on
