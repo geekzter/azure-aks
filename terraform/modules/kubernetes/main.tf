15513f63a5d5ef516e8244b9d1dfd89f716b5266
Eric van Wijk <eric@van-wijk.com>
2022-09-25 10:17:20 +0200

    Remove Kured

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -1,6 +1,6 @@
-# https://weaveworks.github.io/kured/
-resource helm_release kured {
-  name                         = "kured-release"
-  repository                   = "https://weaveworks.github.io/kured/"
-  chart                        = "kured"
-}
+# # https://weaveworks.github.io/kured/
+# resource helm_release kured {
+#   name                         = "kured-release"
+#   repository                   = "https://weaveworks.github.io/kured/"
+#   chart                        = "kured"
+# }
